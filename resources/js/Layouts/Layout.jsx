f354f3f6eecea9c8ca3e6ba550c1723364861fba
ibrahimjml <anony.jjml@gmail.com>
2025-12-14 23:51:28 +0200

    refactor(UI): refactored old UI to new one & added pattern design

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -2,15 +2,14 @@ import { Link, router, usePage } from "@inertiajs/react";
 import { route } from 'ziggy-js';
 import { useState,useEffect } from "react";
 import { ToastContainer } from 'react-toastify';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Layout({children}) {
-  const {auth} = usePage().props;
-  const [showmodel,setshowmodel] = useState(false);
-   const [darkMode, setDarkMode] = useState(() =>
-    localStorage.getItem("theme") === "dark"
-  );
-  const [showHeader, setShowHeader] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
+   const {auth} = usePage().props;
+   const [showmodel,setshowmodel] = useState(false);
+   const [darkMode, setDarkMode] = useState(() => localStorage.getItem("theme") === "dark");
+   const [showHeader, setShowHeader] = useState(true);
+   const [lastScrollY, setLastScrollY] = useState(0);
 
     useEffect(() => {
     document.documentElement.classList.toggle("dark", darkMode);
@@ -34,42 +33,53 @@ useEffect(() => {
   const switchTheme = () => setDarkMode(prev => !prev);
   
   return (
-    <div className="max-w-screen">
-    <header className={`bg-slate-600 dark:bg-gray-800 px-4 py-5 transition-transform duration-300 z-50 w-full fixed top-0 ${showHeader ? "translate-y-0" : "-translate-y-full"}`}>
+    <div className="container lg:max-w-screen w-[80%] mx-auto">
+<header
+  className={`container mx-auto w-[80%] rounded-lg
+    bg-white dark:bg-gray-800/80 
+    backdrop-blur-md backdrop-saturate-150
+    border border-white/20 dark:border-white/10
+    px-4 py-5
+    transition-transform duration-300
+    z-50  fixed top-0
+    ${showHeader ? "translate-y-0" : "-translate-y-full"}
+  `}
+>
         <nav className="flex justify-between items-center gap-4 text-slate-200 font-semibold">
-          <h1 className="text-3xl font-semibold">Inertia Blog</h1>
+          <h1 className="text-3xl font-semibold text-gray-800/80 dark:text-white">Inertia Blog</h1>
           
             {auth.user ? (
-              <div className="space-x-4 pr-5 flex items-center ">
+              <div className="space-x-4 pr-5 flex items-center text-black/40 dark:text-white">
                 <button onClick={switchTheme}>
                   {darkMode ? (
                     <i className="fa-solid fa-sun text-yellow-300"></i>
                   ) : (
-                    <i className="fa-solid fa-moon text-white"></i>
+                    <i className="fa-solid fa-moon text-black/40 dark:text-white"></i>
                   )}
                 
                 </button>
-              <Link href={route('home')} className="hidden lg:block">Home</Link>
+              <Link href={route('home')} className="hidden lg:block text-gray-800/80 dark:text-white">Home</Link>
               {auth?.user.can.access  && 
-                <Link href={route('admin.page')} className="hidden lg:block">Dashboard</Link>
+                <Link href={route('admin.page')} className="hidden lg:block text-gray-800/80 dark:text-white">Admin Panel</Link>
                 }
               <div className="relative flex items-center">
-                <div onClick={togglemodel} className="flex gap-1">
-                <button className="mr-1 hidden lg:block">{auth.user.name}</button>
-                <button className="hidden lg:block">
+                <div onClick={togglemodel} className="flex gap-1 items-center">
+                  <FontAwesomeIcon icon="user" className="hidden lg:block text-sm mr-2 text-gray-800/80 dark:text-white"></FontAwesomeIcon>
+                <button className="mr-1 hidden lg:block text-gray-800/80 dark:text-white">{auth.user.name}</button>
+                <button className="hidden lg:block text-gray-800/80 dark:text-white">
                   <i className="fa-solid fa-caret-down"></i>
                 </button>
                 </div>
                 {showmodel && 
-                <div onClick={togglemodel} className="absolute z-50 top-16  lg:right-0 -right-10 border dark:border-slate-200 bg-slate-600 dark:bg-slate-800 text-white rounded-lg overflow-hidden w-40">
-                  <Link href={route('posts.create')} className="block w-full px-6 py-3 hover:bg-slate-700 text-left">Create Post</Link>
-                  <Link href={route('dashboard')} className="block w-full px-6 py-3 hover:bg-slate-700 text-left">Dashboard</Link>
-                  <Link href={route('edit.profile')} className="block w-full px-6 py-3 hover:bg-slate-700 text-left">Edit Profile</Link>
-                  <Link href={route('home')} className="lg:hidden block w-full px-6 py-3 hover:bg-slate-700 text-left">Home</Link>
+                <div onClick={togglemodel} className="absolute z-50 top-16  -right-4 border dark:border-slate-200 bg-white dark:bg-slate-800 dark:text-white rounded-lg overflow-hidden w-40">
+                  <Link href={route('posts.create')} className="block w-full px-6 py-3 dark:hover:bg-slate-700 hover:bg-gray-200/20 text-left text-gray-800/80 dark:text-white">Create Post</Link>
+                  <Link href={route('dashboard')} className="block w-full px-6 py-3 dark:hover:bg-slate-700 hover:bg-gray-200/20 text-left text-gray-800/80 dark:text-white">Dashboard</Link>
+                  <Link href={route('edit.profile')} className="block w-full px-6 py-3 dark:hover:bg-slate-700 hover:bg-gray-200/20 text-left text-gray-800/80 dark:text-white">Edit Profile</Link>
+                  <Link href={route('home')} className="lg:hidden block w-full px-6 py-3 dark:hover:bg-slate-700 hover:bg-gray-200/20 text-left text-gray-800/80 dark:text-white">Home</Link>
                    {auth.user.role === 'admin' && 
-                  <Link href={route('admin.page')} className="lg:hidden block w-full px-6 py-3 hover:bg-slate-700 text-left">Dashboard</Link>
+                  <Link href={route('admin.page')} className="lg:hidden block w-full px-6 py-3 dark:hover:bg-slate-700 hover:bg-gray-200/20 text-left text-gray-800/80 dark:text-white">Dashboard</Link>
                    }
-                  <button  onClick={handleLogout}  className="block w-full px-6 py-3 hover:bg-slate-700 text-left">Logout</button>
+                  <button  onClick={handleLogout}  className="block w-full px-6 py-3 dark:hover:bg-slate-700 hover:bg-gray-200/20 text-left text-gray-800/80 dark:text-white">Logout</button>
                </div>
                }
               </div>
@@ -95,7 +105,7 @@ useEffect(() => {
         
         </nav>
       </header>
-      <main className="p-4 pt-24">
+      <main className="pt-24">
         {children}
         <ToastContainer position="top-right" autoClose={3000} />
       </main>
